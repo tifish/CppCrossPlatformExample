a40e99dbd44984f451c9621664e4bc936d337a9f
Winston Feng <tifish@gmail.com>
2024-04-11 17:09:56 +0800

    Fix shell script arguments.

--- BuildIOS.sh
+++ BuildIOS.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cmake -B "Build/iOS" -G Xcode -DCMAKE_SYSTEM_NAME=iOS $* &&
+cmake -B "Build/iOS" -G Xcode -DCMAKE_SYSTEM_NAME=iOS "$@" &&
     cmake --build Build/iOS --config Debug &&
     cmake --build Build/iOS --config Release
--- BuildMacOS.sh
+++ BuildMacOS.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cmake -B "Build/Darwin" -G Xcode -DCMAKE_SYSTEM_NAME=Darwin -DDARWIN=TRUE $* &&
+cmake -B "Build/Darwin" -G Xcode -DCMAKE_SYSTEM_NAME=Darwin -DDARWIN=TRUE "$@" &&
     cmake --build Build/Darwin --config Debug &&
     cmake --build Build/Darwin --config Release
