e45c8a43715f3d6a2ab0ce9f9528865ba123367c
Winston Feng <tifish@gmail.com>
2024-05-07 14:50:18 +0800

    Optimize BuildLinux scripts.

--- BuildLinux.cmd
+++ BuildLinux.cmd
@@ -9,11 +9,7 @@ if errorlevel 1 (
     exit /b
 )
 
-wsl -- ^
-export PATH=$PWD/Tools/cmake/bin:$PWD/Tools/ninja:$PATH ^&^& ^
-cmake -B "Build/Linux" -G "Ninja Multi-Config" -DCMAKE_SYSTEM_NAME=Linux %* ^&^& ^
-cmake --build Build/Linux --config Debug ^&^&^
-cmake --build Build/Linux --config Release ||^
+wsl -- ./BuildLinux.sh %* ||^
 pause
 
 endlocal
--- BuildLinux.sh
+++ BuildLinux.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 export PATH=$PWD/Tools/cmake/bin:$PWD/Tools/ninja:$PATH &&
-cmake -B "Build/Linux" -G "Ninja Multi-Config" -DCMAKE_SYSTEM_NAME=Linux "$@" &&
-cmake --build Build/Linux --config Debug &&
-cmake --build Build/Linux --config Release ||
-pause
-
+    cmake -B "Build/Linux" -G "Ninja Multi-Config" -DCMAKE_SYSTEM_NAME=Linux "$@" &&
+    cmake --build Build/Linux --config Debug &&
+    cmake --build Build/Linux --config Release
