5d4e6be48894b5b7e959504a7cf3b1315d26dea6
root <root@fengmingyupc.localdomain>
2024-05-07 11:59:23 +0800

    Add BuildLinux.sh

---
+++ BuildLinux.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export PATH=$PWD/Tools/cmake/bin:$PWD/Tools/ninja:$PATH &&
+cmake -B "Build/Linux" -G "Ninja Multi-Config" -DCMAKE_SYSTEM_NAME=Linux "$@" &&
+cmake --build Build/Linux --config Debug &&
+cmake --build Build/Linux --config Release ||
+pause
+
